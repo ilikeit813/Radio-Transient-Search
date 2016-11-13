137ee3935dbf678092242540bf7bc0cdff32fdbc
Jr Wei Tsai (Jamie) <ilikeit813@qq.com>
2016-11-14 10:28:29 +1100

    Update cadiplot.py

--- cadiplot.py
+++ cadiplot.py
@@ -1,13 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
 
 def snr(a):
     return (a-a.mean() )/a.std()
+tint = 0.00020897959
+fn = glob.glob('ca*.npy')
+print 'ploting file =',fn[0], 'offset time =', float(fn[0][35:44])/4*tint, ' sec'
+offset = float(fn[0][35:44])/4*tint
+sp = np.load(fn[0])
 
-sp = np.load('candi057139_000656029_1_fft_offset_029943115_frames.npy')
+
+plt.suptitle('Spectrogram Low Tuning Offset Time %s sec' % str(offset), fontsize = 15)
+
+plt.subplot(2, 2, 3)
 plt.imshow(snr(sp[:,0,:]).T,origin = 'low',aspect ='auto',cmap='Greys_r')
-plt.suptitle('Spectrogram Low Tuning', fontsize = 30)
-plt.xlabel('Time (0.84 ms)',fontdict={'fontsize':16})
-plt.ylabel('Frequency (1.2 kHz)',fontdict={'fontsize':14})
-plt.colorbar().set_label('std',size=18)
+plt.xlabel('Time (0.84 ms)')
+plt.ylabel('Frequency (1.2 kHz)')
+plt.colorbar().set_label('std')
+plt.locator_params(nbins=8)
+
+plt.subplot(2, 2, 1)
+plt.plot(snr(sp[:,0,:]).T.mean(0))
+plt.xlabel('Time series (0.84 ms)')
+plt.ylabel('Arbitrary Units')
+plt.locator_params(nbins=4)
+
+plt.subplot(2, 2, 4)
+plt.plot(snr(sp[:,0,:]).T.mean(1),np.arange(sp.shape[2]))
+plt.xlabel('Bandpass (Arbitrary Units)')
+plt.ylabel('Frequency (1.2 kHz)')
+plt.locator_params(nbins=4)
+
+
+plt.tight_layout()
+plt.subplots_adjust(top=0.9)
 plt.savefig('cadi')
