25121add4809a8de1945e92c4dc132341818e416
Jr Wei Tsai (Jamie) <ilikeit813@qq.com>
2016-11-13 19:30:25 +1100

    Create interpolate.py

---
+++ interpolate.py
@@ -0,0 +1,19 @@
+import numpy
+import glob
+
+fn = sorted(glob.glob('05*.npy'))
+j = numpy.zeros((len(fn)))
+for i in range(len(fn)):
+        j[i] = fn[i][30:39]
+
+nChunks_nFramesAvg = j[1]-j[0]
+#x = total perfect offset
+x = numpy.arange(j[-1]/nChunks_nFramesAvg)*nChunks_nFramesAvg
+# k = the different between perfect and real
+k = numpy.setdiff1d(x, j)
+
+for i in range(len(k)):
+    a=fn[numpy.where( j == k[i]-nChunks_nFramesAvg)[0]]
+    b=fn[numpy.where( j == k[i]+nChunks_nFramesAvg)[0]]
+    newfn = fn[0][0:30]+'%s' % int(k[i]) +fn[0][39:]
+    numpy.save(newfn,numpy.load(a)*.5+numpy.load(b)*.5)
