bf883cd2ae0512214ddc71e3f8ef54e0662942c6
Jr Wei Tsai (Jamie) <ilikeit813@qq.com>
2016-11-13 14:32:21 +1100

    Update ft.py

--- ft.py
+++ ft.py
@@ -1,6 +1,4 @@
-#Updates, independent from LSL
-#Sugestion, split voltage raw data in to chunks so each processer can read the file parallelly
-
+#from mpi4py import MPI
 import mpi4py 
 import mpi4py.MPI as MPI
 import os
@@ -12,18 +10,24 @@ import time
 import matplotlib.pyplot as plt
 
 def main(args):
-        totalrank = 12
+	nodes = 4
+	pps = 6
+        totalrank = nodes*pps
         comm  = MPI.COMM_WORLD
         rank  = comm.Get_rank()
 	t0 = time.time()
 	nChunks = 3000 #the temporal shape of a file.
-	LFFT = 4096*16 #Length of the FFT.4096 is the size of a frame readed.
+	LFFT = 4096*4 #Length of the FFT.4096 is the size of a frame readed.
 	nFramesAvg = 1*4*LFFT/4096 # the intergration time under LFFT, 4 = beampols = 2X + 2Y (high and low tunes)
-	fcl = 6000+7000
-	fch = fcl + 3343 #60000
+	#Low tuning frequency range
+	Lfcl = 1700*4
+	Lfch = 2100*4
+	#High tuning frequency range
+	Hfcl =  670*4
+	Hfch = 1070*4
 	
 	#for offset_i in range(4306, 4309):# one offset = nChunks*nFramesAvg skiped
-	for offset_i in range(0, 1 ):# one offset = nChunks*nFramesAvg skiped
+	for offset_i in range(0, 1000 ):# one offset = nChunks*nFramesAvg skiped
                 offset_i = 1.*totalrank*offset_i + rank
 		offset = nChunks*nFramesAvg*offset_i
 		# Build the DRX file
@@ -79,14 +83,14 @@ def main(args):
 		if nFrames > (nFramesFile - offset):
 			raise RuntimeError("Requested integration time + offset is greater than file length")
 		# Master loop over all of the file chunks
-		freq = numpy.fft.fftshift(numpy.fft.fftfreq(LFFT, d = 1.0/srate))
-		tInt = 1.0*LFFT/srate
-                print 'Temporal resl = ',tInt
-                print 'Channel width = ',1./tInt
-		freq1 = freq+centralFreq1
-		freq2 = freq+centralFreq2
+		#freq = numpy.fft.fftshift(numpy.fft.fftfreq(LFFT, d = 1.0/srate))
+		#tInt = 1.0*LFFT/srate
+                #print 'Temporal resl = ',tInt
+                #print 'Channel width = ',1./tInt
+		#freq1 = freq+centralFreq1
+		#freq2 = freq+centralFreq2
 		#print tInt,freq1.mean(),freq2.mean()
-		masterSpectra = numpy.zeros((nChunks, 2, fch-fcl))
+		masterSpectra = numpy.zeros((nChunks, 2, Lfch-Lfcl))
 		for i in xrange(nChunks):
 			# Find out how many frames remain in the file.  If this number is larger
 			# than the maximum of frames we can work with at a time (nFramesAvg),
@@ -121,22 +125,11 @@ def main(args):
 				aStand = 2*(tune-1) + pol
 				data[aStand, count[aStand]*4096:(count[aStand]+1)*4096] = cFrame.data.iq
 				count[aStand] +=  1
-			# Calculate the spectra for this block of data
-			#tempSpec1 = numpy.fft.fftshift(numpy.abs(numpy.fft.fft2(data[:2,:]))[:,1:]/2.)[:,fcl:fch].mean(0)**2./LFFT*2. #in unit of energy
-			#masterSpectra[i,0,:] = numpy.fft.fftshift(numpy.abs(numpy.fft.fft2(data[:2,:]))[:,1:])[:,fcl:fch].mean(0)**2./LFFT/2. #in unit of energy
-			masterSpectra[i,0,:] = ((numpy.fft.fftshift(numpy.abs(numpy.fft.fft2(data[:2,:]))[:,1:])[:,fcl:fch])**2.).mean(0)/LFFT/2. #in unit of energy
-			#tempSpec2 = numpy.fft.fftshift(numpy.abs(numpy.fft.fft2(data[2:,:]))[:,1:]/2.)[:,fcl:fch].mean(0)**2./LFFT*2. #in unit of energy
-			masterSpectra[i,1,:] = ((numpy.fft.fftshift(numpy.abs(numpy.fft.fft2(data[2:,:]))[:,1:])[:,fcl:fch])**2.).mean(0)/LFFT/2. #in unit of energy
-			# Save the results to the various master arrays
-			#print masterSpectra.shape
-			#numpy.save('data',data)
-			#sys.exit()
-			if i % 100 ==1 :
-				print i, ' / ', nChunks
+			# Calculate the spectra for this block of data, in the unit of intensity
+			masterSpectra[i,0,:] = ((numpy.fft.fftshift(numpy.abs(numpy.fft.fft2(data[:2,:]))[:,1:])[:,Lfcl:Lfch])**2.).mean(0)/LFFT/2.
+			masterSpectra[i,1,:] = ((numpy.fft.fftshift(numpy.abs(numpy.fft.fft2(data[2:,:]))[:,1:])[:,Hfcl:Hfch])**2.).mean(0)/LFFT/2.
+		# Save the results to the various master arrays
                 outname = "%s_%i_fft_offset_%.9i_frames" % (getopt.getopt(args,':')[1][0], beam,offset)
-		numpy.save('jamie'+outname,masterSpectra)
-	print time.time()-t0
-	print masterSpectra.shape
-	#print masterSpectra.shape
+		numpy.save(outname,masterSpectra)
 if __name__ == "__main__":
 	main(sys.argv[1:])
