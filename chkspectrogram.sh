2045db38e3132a1fd803848fba7d7950e080ca60
Jr Wei Tsai (Jamie) <ilikeit813@qq.com>
2016-11-13 17:15:10 +1100

    Create chkspectrogram.sh

---
+++ chkspectrogram.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l nodes=1:ppn=6
+#PBS -W group_list=hokieone
+#PBS -q normal_q
+#PBS -A hokieone
+
+# Add any the intel compiler and MPT MPI modules
+#module reset
+#module load mkl mpiblast python
+#module swap intel gcc
+#module load mkl python
+#module load mkl mpt python
+#module load intel mpt
+#module add mpiblast
+#module load openmpi
+
+#module load mkl mpiblast python
+
+module reset
+#module swap openmpi
+#module swap mvapich2 openmpi
+module load mkl python openmpi
+
+
+
+
+cd /work/hokieone/ilikeit/057139_000656029
+cp /home/ilikeit/hokieone/chkspectrogram.py .
+cp /home/ilikeit/hokieone/errors.py .
+cp /home/ilikeit/hokieone/drx.py .
+cp /home/ilikeit/hokieone/dp.py .
+
+mpirun -np $PBS_NP python chkspectrogram.py 057139_000656029
+echo "done"
+exit;
