a32e1024bbb3d401bf58dc0e59f59a3b3bb53270
Jr Wei Tsai (Jamie) <ilikeit813@qq.com>
2016-11-13 14:33:00 +1100

    Update ft.sh

--- ft.sh
+++ ft.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
-#PBS -l walltime=48:00:00
-#PBS -l nodes=1:ppn=16
-#PBS -W group_list=blueridge
+#PBS -l walltime=24:00:00
+#PBS -l nodes=4:ppn=6
+#PBS -W group_list=hokieone
 #PBS -q normal_q
-#PBS -A LWAradio
-#PBS -M ilikeit@vt.edu
-#PBS -m bea
+#PBS -A hokieone
+
+# Add any the intel compiler and MPT MPI modules
+#module reset
+#module load mkl mpiblast python
+#module swap intel gcc
+#module load mkl python
+#module load mkl mpt python
+#module load intel mpt
+#module add mpiblast 
+#module load openmpi
+
+#module load mkl mpiblast python
 
 module reset
-module swap mvapich2 openmpi
-module load mkl python
+#module swap openmpi
+#module swap mvapich2 openmpi
+module load mkl python openmpi
 
-cd /work/blueridge/ilikeit/057139_000656029/
-cp /home/ilikeit/ft.py .
-cp /home/ilikeit/errors.py .
-cp /home/ilikeit/drx.py .
-cp /home/ilikeit/dp.py .
-#rm *.txt
-mpirun -np $PBS_NP python ft.py 057139_000656029 1
 
-echo "------------------------------------------"
-echo "done! with cores =" $PBS_NP
-echo "------------------------------------------"
 
+
+cd /work/hokieone/ilikeit/057139_000656029
+cp /home/ilikeit/hokieone/ft.py .
+cp /home/ilikeit/hokieone/errors.py .
+cp /home/ilikeit/hokieone/drx.py .
+cp /home/ilikeit/hokieone/dp.py .
+
+mpirun -np $PBS_NP python ft.py 057139_000656029
+
+echo "done"
 exit;
