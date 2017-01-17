0a870995b7ed7015372ae6c01d575822f2125e2f
Jr Wei Tsai (Jamie) <ilikeit813@qq.com>
2017-01-17 17:36:42 +1100

    Update interpolate.py

--- interpolate.py
+++ interpolate.py
@@ -16,4 +16,5 @@ for i in range(len(k)):
     a=fn[numpy.where( j == k[i]-nChunks_nFramesAvg)[0]]
     b=fn[numpy.where( j == k[i]+nChunks_nFramesAvg)[0]]
     newfn = fn[0][0:30]+'%.9i' % int(k[i]) +fn[0][39:]
-    numpy.save(newfn,numpy.load(a)*.5+numpy.load(b)*.5)
+    print newfn
+    #numpy.save(newfn,numpy.load(a)*.5+numpy.load(b)*.5)
