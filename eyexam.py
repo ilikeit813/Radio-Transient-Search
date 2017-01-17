10231da45a3f41b2787e052c7c313764ce93e8b3
Jr Wei Tsai (Jamie) <ilikeit813@qq.com>
2017-01-17 16:42:32 +1100

    Update eyexam.py

--- eyexam.py
+++ eyexam.py
@@ -4,15 +4,22 @@ Use this code to examine if there are missing files.
 import numpy
 import glob
 
+windownumber = 2
 nodes = 2
 pps = 6
-nChunks = 10000
-nFramesAvg = 4
+nChunks = 1000
+nFramesAvg = 4*windownumber
 
-fn = sorted(glob.glob('waterfall05*.npy'))
+#fn = sorted(glob.glob('waterfall05*.npy'))
+#j = numpy.zeros((len(fn)))
+#for i in range(len(fn)):
+#        j[i] = fn[i][39:48]
+
+fn = sorted(glob.glob('05*.npy'))
 j = numpy.zeros((len(fn)))
 for i in range(len(fn)):
-        j[i] = fn[i][39:48]
+        j[i] = fn[i][30:39]
 
 x = numpy.arange(j[-1]/nChunks/nFramesAvg)*nChunks*nFramesAvg
 offset = numpy.setdiff1d(x, j)
+print 'missing ', offset.shape, 'files'
