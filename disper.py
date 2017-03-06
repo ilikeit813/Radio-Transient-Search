df325c20730016f20e453d3482a1e6e37e49a336
Jr Wei Tsai (Jamie) <ilikeit813@qq.com>
2017-03-06 14:30:07 +1100

    Updated disper.py

--- disper.py
+++ disper.py
@@ -15,23 +15,34 @@ def S_delatDM_vs_S_ratio(kersci):
 def dispersion_lag_second(DM, nuLowMHz, nuHighMHz):
     return DM*4.148808*10**3*((1./nuLowMHz)**2-(1./nuHighMHz)**2)
 
-def dDMi(DMtrial = 1., nuCenteralMHz = 2., channelMHz = 0.1, BMHz = 1., SSratio = 0.95, temporal_resol = 0.00021):
-    dDM = 0.1
+def dDMi(DMtrial = 1., nuCenteralMHz = 2., channelMHz = 0.1, BMHz = 1., SSratio = 0.95, temporal_resol = 0.00021, pulsewidth = 0.01):
+    dDM = 0.001
     w_sec = dispersion_lag_second(DM = DMtrial, nuLowMHz = nuCenteralMHz - 0*BMHz/2 - channelMHz/2, nuHighMHz =  nuCenteralMHz - 0*BMHz/2 + channelMHz/2)
-    if w_sec<temporal_resol: w_sec = temporal_resol
+    w_sec = np.array([pulsewidth, temporal_resol, w_sec]).max()
+    print 'Un-de-dispersed-able width', w_sec, 'sec', '(temporal resol. =', temporal_resol, 'sec)'
     k = kersci(deltaDM = 1.*dDM, BandwidthMHz = 1.*BMHz, width_ms = 1.*w_sec*1000, nuGHz = 1.*nuCenteralMHz/1000)
-    #print 'Hi, is ',np.abs(S_delatDM_vs_S_ratio(k) - SSratio),"< 0.001 ?"
     counter = 0
-    while S_delatDM_vs_S_ratio(k) - SSratio > 0.000001:
-        if DMtrial == 0:
-            return 0.1
-        elif DMtrial < 100 : dDM = 0.1
-        else: dDM = 0.0001*DMtrial
-        w_sec = dispersion_lag_second(DM = DMtrial, nuLowMHz = nuCenteralMHz - 0*BMHz/2 - channelMHz/2, nuHighMHz =  nuCenteralMHz - 0*BMHz/2 + channelMHz/2)
-        if w_sec<temporal_resol: w_sec = temporal_resol
+    if S_delatDM_vs_S_ratio(k) <= SSratio: return dDM
+    while S_delatDM_vs_S_ratio(k) > SSratio: # > 0.01*SSratio:
+        dDM += 0.001
         k = kersci(deltaDM = 1.*dDM, BandwidthMHz = 1.*BMHz, width_ms = 1.*w_sec*1000, nuGHz = 1.*nuCenteralMHz/1000)
         counter += 1
-        if counter > 100: break
-    if dDM < 0.1:
-        return 0.1
-    else: return dDM
+        if counter>10000:
+            break
+    return(dDM)
+
+def main(args):
+    ssratio         = 0.5
+    pulse_width     = 0.089328385024/512*40
+    DM              = 69.                   #pc/cc
+    centeralfreq    = 835.5957031           #MHz
+    bandwidth       = 31.25                 #MHz
+    tInt            = 0.0003276798693344222 #sec
+    numberOfchannel = 320.                  #bins
+    print "pulse width = ", pulse_width, "sec"
+    print 'channel width', 1.*bandwidth/numberOfchannel*1000, 'kHz'
+    ddm = dDMi(1.*DM, 1.*centeralfreq, 1.*bandwidth/numberOfchannel, 1.*bandwidth, 1.*ssratio, 1.*tInt, 1.*pulse_width)
+    print 'DM error = ',ddm, 'pc/cc, S/R ratio =', ssratio
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
