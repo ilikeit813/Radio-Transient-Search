4b03da6eb182e44e5960616d47d3b42eb006fb23
Cregg Yancey <cyancey@terpmail.umd.edu>
2018-02-26 09:29:56 -0700

    - In waterfall.sh:   Added the ability for user to specify the full path to the data from the command-line.
      Added additional user feedback.
    
      Added usage message.
    
      Changed value of NUM_PROCS from 1 to 0.
    
    - In chkwaterfall.sh
      Added the ability for user to specify the full path to the data from the command-line.
    
      Added additional user feedback.
    
      Added usage message.
    
      Added multi-pass at user's request.

--- chkwaterfall.sh
+++ chkwaterfall.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+# CCY - Added OPT-INSTALL_DIR as a template variable that is replaced by my git-export.sh script with
+# the path to which the package is installed.  If you don't have my git-export.sh script, then you can
+# just manually replace all instances of OPT-INSTALL_DIR as necessary.
+#
+
+USAGE='
+   
+   chkwaterfall.sh [ --help | -h ] <data_path>
+
+   Script to call chkwaterfall.py that checks whether there are any missing frames output from a
+   previous call to waterfall.py(sh) for the data file specified by <data_path>.  Multiple passes of
+   chkwaterfall.py can be applied by answering 'y' or 'yes' when asked if the user wants to do another
+   pass.
+
+   OPTIONS:
+   --help | -h : Display this help message.
+'
+
 #PBS -l walltime=24:00:00
 #PBS -l nodes=2:ppn=6
 #PBS -W group_list=hokieone
@@ -17,19 +35,58 @@
 
 #module load mkl mpiblast python
 
-module reset
+#module reset
 #module swap openmpi
 #module swap mvapich2 openmpi
-module load mkl python openmpi
+#module load mkl python openmpi
+NUM_PROCS=0
 
 
-cd /work/hokieone/ilikeit/057139_000656029
-cp /home/ilikeit/hokieone/chkwaterfall.py .
-cp /home/ilikeit/hokieone/errors.py .
-cp /home/ilikeit/hokieone/drx.py .
-cp /home/ilikeit/hokieone/dp.py .
 
-mpirun -np $PBS_NP python OPT-INSTALL_DIR/chkwaterfall.py 057139_000656029
+# Path to the data file.
+DATA_PATH=
 
-echo "done"
-exit;
+# Get user input from command-line.
+while [ -z "${DATA_PATH}" -a -n "$1" ]
+do
+   case  "$1"
+   in
+      --help | - h) # Display usage and then exit
+         echo "${USAGE}"
+         exit 0
+      -*) # Any other option flag is ignored
+         echo "UNKNOWN OPTION: $1"
+         echo "${USAGE}"
+         exit 1
+      *) # Use the first non-option flag as the data path.
+         DATA_PATH="$1"
+         shift
+         ;;
+   esac
+done
+
+if [ -n "${DATA_PATH}" ]; then
+   echo "running chkwaterfall.py with DATA_PATH=${DATA_PATH}" FLAG_DOPASS=1
+   while [ ${FLAG_DOPASS} -eq 1 ]
+   do
+      mpirun -np ${NUM_PROCS} python OPT-INSTALL_DIR/chkwaterfall.py "${DATA_PATH}"
+      # Ask user whether to do another pass.
+      echo "Perform another pass? [y/n]"
+      read ANSWER
+      case `echo "${ANSWER}" | tr "[:upper:]" "[:lower:]"`
+      in
+         y | yes | yup | yeah | ya | yea) # Perform another pass
+         ;;
+         *) # Otherwise, stop.
+         FLAG_DOPASS=0
+         ;;
+      esac
+   done
+   echo "done"
+else
+   echo "No data path specified"
+   echo "${USAGE}"
+   exit 1
+fi
+
+exit 0;
--- waterfall.sh
+++ waterfall.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 # CCY - Added OPT-INSTALL_DIR as a template variable that is replaced by my git-export.sh script with
-# the path to which the package is installed.
+# the path to which the package is installed.  If you don't have my git-export.sh script, then you can
+# just manually replace all instances of OPT-INSTALL_DIR as necessary.
 #
+
+USAGE='waterfall.sh
+
+   waterfall.sh [--help | -h] <data_path>
+
+   Script calling the waterfall.py python script in the installed package of radiotrans to operate on a
+   radio time series specified by <data_path>.  The results of waterfall.py are output to the current
+   working directory.
+
+   OPTIONS:
+   --help | -h : Display this help message.
+  ' 
+
 #PBS -l walltime=24:00:00
 #PBS -l nodes=2:ppn=6
 #PBS -W group_list=hokieone
@@ -20,17 +34,44 @@
 
 #module load mkl mpiblast python
 
-module reset
+#module reset
 #module swap openmpi
 #module swap mvapich2 openmpi
-module load mkl python openmpi
-NUM_PROCS=1
+#module load mkl python openmpi
+NUM_PROCS=0
 
-DATA_DIR="/data/network/recent_data/jtsai"
-DATA_FILENAME="057974_001488582"
-DATA_PATH="${DATA_DIR}/${DATA_FILENAME}"
 
-mpirun -np ${NUM_PROCS} python OPT-INSTALL_DIR/waterfall.py "${DATA_PATH}"
 
-echo "done"
-exit;
+# Path to the data file.
+DATA_PATH=
+
+# Get user input from command-line.
+while [ -z "${DATA_PATH}" -a -n "$1" ]
+do
+   case  "$1"
+   in
+      --help | - h) # Display usage and then exit
+         echo "${USAGE}"
+         exit 0
+      -*) # Any other option flag is ignored
+         echo "UNKNOWN OPTION: $1"
+         echo "${USAGE}"
+         exit 1
+      *) # Use the first non-option flag as the data path.
+         DATA_PATH="$1"
+         shift
+         ;;
+   esac
+done
+
+if [ -n "${DATA_PATH}" ]; then
+   echo "running waterfall.py with DATA_PATH=${DATA_PATH}"
+   mpirun -np ${NUM_PROCS} python OPT-INSTALL_DIR/waterfall.py "${DATA_PATH}"
+   echo "done"
+else
+   echo "No data path specified"
+   echo "${USAGE}"
+   exit 1
+fi
+
+exit 0;
